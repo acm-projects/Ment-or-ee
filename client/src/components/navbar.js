fce9cfe85b8edb4e2d2d2d38b3b2bc4fe281cb93
masorol <miasorola.yoshida@gmail.com>
2024-09-23 21:13:24 -0500

    rough landing page

---
+++ client/src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import logo from '../assets/logo.png'  
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-[80px] bg-white border-b border-gray-300 shadow-sm">
+      <div className="max-w-[1480px] mx-auto w-full h-full flex justify-between items-center px-4">
+        {/* <img src={logo} className="h-[25px]" /> */}
+        <div className='flex items-center'>
+          <ul className="flex space-x-4">
+            <li>Home</li>
+            <li>Matches</li>
+            <li>Tasks</li>
+            <li>Chat</li>
+            <li>Profile</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
+
